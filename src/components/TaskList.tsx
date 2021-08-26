9e5ffe5855531296559fa320dbd408a9baabb424
gabriel silva <gabrielp.siilva@gmail.com>
2021-08-26 12:39:42 -0300

    finalizando desafio 01 - ignite react

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,14 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    if(newTaskTitle) {
+      let newTask: Task = {
+        id: Math.floor(Math.random() * 1000),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+  
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle('');
+    }
+    return;
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
   }
 
   function handleToggleTaskCompletion(id: number) {
+    let [ task ] = tasks.filter(task => task.id === id);
+    task.isComplete = !task.isComplete;
+    setTasks([...tasks]);
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   }
 
   function handleRemoveTask(id: number) {
+    let taskListUpdated = tasks.filter(task => task.id !== id);
+    setTasks(taskListUpdated);
     // Remova uma task da listagem pelo ID
   }
 
